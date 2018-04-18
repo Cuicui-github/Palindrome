92fb2d2b064bf8f7203cb4a6e541e76c9274fdbe
ceeceecuicui <hwwlcc@gmail.com>
2018-04-17 23:40:56 -0700

    Add files via upload

---
+++ zqxt_tmpl/learn/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    
+    return render(request,'home.html')
+def isPalindrome(request):
+
+    r=request.GET['input_string']
+    string1 = r.replace(" ","")
+    test_string=string1.lower()
+    rev = ''.join(reversed(test_string))
+    if (test_string == rev):
+        string2=r+": Yes"
+    else:
+        string2=r+": No"
+    f=open('history.txt','a')
+    f.write(string2+';')
+    f.close()
+    f=open('history.txt','r')
+    string=f.read()
+    return render(request,'home.html',{'string':string})
+    f.close()
+    
+
+
